144ba173bf1134eac484522b5d392e7ee0d737f2
irfandeen <abdulwahabirfan1@gmail.com>
2025-03-18 02:13:43 +0800

    Remove data.txt from worktree

--- src/test/java/seedu/duke/storage/StorageTest.java
+++ src/test/java/seedu/duke/storage/StorageTest.java
@@ -1,12 +1,31 @@
 package seedu.duke.storage;
 
 import org.junit.jupiter.api.Test;
+import seedu.duke.model.ApplicationStatus;
+import seedu.duke.model.InternshipApplication;
+import seedu.duke.storage.exceptions.StorageException;
 
 class StorageTest {
-
     @Test
     // happy path
     void readApplicationsFromFile_fileExists_success() {
 
+    }
+
+    @Test
+    // happy path
+    void storeApplicationsToFile() {
+        Storage storage = new StorageManager();
+
+        InternshipApplication[] applications = new InternshipApplication[3];
+        for (int i = 0; i < 3; i++) {
+            applications[i] = new InternshipApplication("Google", "SWE", ApplicationStatus.APPLIED);
+        }
+
+        try {
+            storage.storeApplicationsToFile(applications);
+        } catch (StorageException e) {
+
+        }
     }
 }
