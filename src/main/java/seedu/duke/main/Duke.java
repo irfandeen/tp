27e9017b54c1bb83618823229bdba041395c240b
irfandeen <abdulwahabirfan1@gmail.com>
2025-03-18 01:28:53 +0800

    Tidy up Duke.java

--- src/main/java/seedu/duke/main/Duke.java
+++ src/main/java/seedu/duke/main/Duke.java
@@ -1,16 +1,6 @@
 package seedu.duke.main;
 
-import seedu.duke.model.ApplicationStatus;
-import seedu.duke.model.InternshipApplication;
-import seedu.duke.storage.Storage;
-import seedu.duke.storage.StorageManager;
-import seedu.duke.storage.exceptions.InvalidDelimitedStringException;
-import seedu.duke.storage.exceptions.StorageException;
 import seedu.duke.ui.UiMain;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Duke {
 
