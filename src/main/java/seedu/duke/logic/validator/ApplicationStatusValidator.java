c895feb0c69ad3c33ed099d86a63df1eccf546d1
zexueteh <66759893+zexueteh@users.noreply.github.com>
2025-03-18 19:34:02 +0800

    Add Junit tests for validators - Test valid, invalid and null inputs - Add defensive programming to validators

--- src/main/java/seedu/duke/logic/validator/ApplicationStatusValidator.java
+++ src/main/java/seedu/duke/logic/validator/ApplicationStatusValidator.java
@@ -1,4 +1,58 @@
 package seedu.duke.logic.validator;
 
-public class ApplicationStatusValidator {
+import static java.util.Objects.requireNonNull;
+
+import seedu.duke.model.ApplicationStatus;
+import seedu.duke.model.exceptions.InvalidApplicationStatus;
+
+/**
+ * Validates application status input.
+ * Ensures the status is either a valid enum name or an integer representing the enum index.
+ */
+public class ApplicationStatusValidator implements Validator<String>{
+
+    private static ApplicationStatusValidator instance;
+
+    @Override
+    public boolean validate(String applicationStatusString) {
+        requireNonNull(applicationStatusString);
+        String trimmedStatus = applicationStatusString.trim();
+        if (trimmedStatus.isEmpty()) {
+            return false;
+        }
+
+        return isValidString(trimmedStatus) || isValidEnum(trimmedStatus);
+    }
+
+    /**
+     * Checks if input can be directly cast to ApplicationStatus
+     */
+    private boolean isValidString(String status) {
+        try {
+            ApplicationStatus.stringToStatus(status);
+            return true;
+        } catch (InvalidApplicationStatus e) {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if input can be cast to integer corresponding to ApplicationStatus enum
+     */
+    private boolean isValidEnum (String status) {
+        try {
+            int index = Integer.parseInt(status);
+            return index >= 0 && index < ApplicationStatus.values().length;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static ApplicationStatusValidator getInstance() {
+        if (instance == null) {
+            instance = new ApplicationStatusValidator();
+        }
+        return instance;
+    }
+
 }
--- src/main/java/seedu/duke/logic/validator/CompanyNameValidator.java
+++ src/main/java/seedu/duke/logic/validator/CompanyNameValidator.java
@@ -1,4 +1,32 @@
 package seedu.duke.logic.validator;
+import static java.util.Objects.requireNonNull;
 
-public class NameValidator {
+/**
+ * Validates company name input.
+ * Ensures it follows the allowed character set and length constraints.
+ */
+public class CompanyNameValidator implements Validator<String> {
+    private static CompanyNameValidator instance;
+
+    /**
+     * Regex to validate Company Name.
+     * Allows alphanumeric characters and special symbols: & , - . ' / ( ). Limit of 100 characters.
+     */
+    private static final String COMPANY_NAME_REGEX = "^[a-zA-Z0-9&,\\-.'/() ]{1,100}$";
+
+
+    @Override
+    public boolean validate(String companyName) {
+        requireNonNull(companyName);
+        String companyNameTrimmed = companyName.trim();
+
+        return !companyNameTrimmed.isEmpty() && companyNameTrimmed.matches(COMPANY_NAME_REGEX);
+    }
+
+    public static CompanyNameValidator getInstance() {
+        if (instance == null) {
+            instance = new CompanyNameValidator();
+        }
+        return instance;
+    }
 }
