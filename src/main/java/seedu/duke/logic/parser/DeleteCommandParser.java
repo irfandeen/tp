1b000a57fe29c245179daaa883ac02f420a82c32
irfandeen <abdulwahabirfan1@gmail.com>
2025-03-19 20:42:15 +0800

    Implement parsing for Delete command
    Delete command will be validated for its index by helper method in
    ParserUtil, which will invoke a validator for job indices.

---
+++ src/main/java/seedu/duke/logic/parser/DeleteCommandParser.java
@@ -0,0 +1,24 @@
+package seedu.duke.logic.parser;
+
+import seedu.duke.logic.commands.DeleteCommand;
+import seedu.duke.logic.parser.exceptions.ParseException;
+
+import static seedu.duke.logic.parser.ParserUtil.parseJobApplicationIndex;
+
+public class DeleteCommandParser implements Parser<DeleteCommand>{
+    /**
+     * Parses the delete command. Delete takes in index of job application, and deletes it.
+     * @param input Index to delete it if valid
+     * @return DeleteCommand object
+     * @throws ParseException
+     */
+    @Override
+    public DeleteCommand parse(String indexString) throws ParseException {
+        int targetIndex = parseJobApplicationIndex(indexString);
+        return new DeleteCommand(targetIndex);
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
