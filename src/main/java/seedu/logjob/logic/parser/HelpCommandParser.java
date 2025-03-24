8f7d52c856b68cdff56c92f0f5d572dfbe42118f
zexueteh <66759893+zexueteh@users.noreply.github.com>
2025-03-24 09:50:18 +0800

    Refactor ApplicationParser and Help, List, Exit Command Parsers to remove regex parsing

--- src/main/java/seedu/logjob/logic/parser/HelpCommandParser.java
+++ src/main/java/seedu/logjob/logic/parser/HelpCommandParser.java
@@ -6,7 +6,7 @@ import seedu.logjob.logic.parser.exceptions.ParseException;
 public class HelpCommandParser implements Parser<HelpCommand>{
     @Override
     public HelpCommand parse(String args) throws ParseException {
-        if (args != null) {
+        if (args == null || !args.trim().isEmpty()) {
             throw new ParseException("Invalid arguments: " + args);
         }
 
