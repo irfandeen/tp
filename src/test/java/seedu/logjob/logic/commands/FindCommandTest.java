c45e855bb921d2cfee269e62f6513ee0d7dd1bec
irfandeen <abdulwahabirfan1@gmail.com>
2025-04-03 21:44:13 +0800

    Add FindCommandParser JUnit tests

---
+++ src/test/java/seedu/logjob/logic/commands/FindCommandTest.java
@@ -0,0 +1,15 @@
+package seedu.logjob.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindCommandTest {
+
+    @Test
+    // happy path
+    void execute_existentKeywords_success() {
+        FindCommand findCommand = new FindCommand("keyword");
+        Dummy
+    }
+}
---
+++ src/test/java/seedu/logjob/logic/parser/FindCommandParserTest.java
@@ -0,0 +1,29 @@
+package seedu.logjob.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.logjob.logic.commands.FindCommand;
+import seedu.logjob.logic.parser.exceptions.ParseException;
+
+import static seedu.logjob.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.logjob.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+class FindCommandParserTest {
+    @Test
+    // happy path
+    void parse_validArgs_throwsParseException() throws ParseException {
+        FindCommandParser parser = new FindCommandParser();
+        assertParseSuccess(parser, "keyword", new FindCommand("keyword"));
+        assertParseSuccess(parser, "keyword     keyword", new FindCommand("keyword     keyword"));
+        assertParseSuccess(parser, "    trailing keyword ", new FindCommand("trailing keyword"));
+    }
+
+    @Test
+    // error path
+    void parse_emptyArgs_throwsParseException() {
+        String EMPTY_PREAMBLE =
+                "Find command expects search term as preamble.\nExample usage: find ABC Bank";
+        FindCommandParser parser = new FindCommandParser();
+        assertParseFailure(parser, "", EMPTY_PREAMBLE);
+        assertParseFailure(parser, "    ", EMPTY_PREAMBLE);
+    }
+}
